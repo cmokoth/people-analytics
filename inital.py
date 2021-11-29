b02ad9fc7377d80d3f12a4240c5a31b0826c800b
cmokoth <cmokoth@ncsu.edu>
2021-11-28 21:19:04 -0500

    initial

---
+++ inital.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+"""
+Header
+"""
+
+#=============================================================================
+# IMPORT STATEMENTS
+#=============================================================================
+
+
+#=============================================================================
+# MODULE-LEVEL VARIABLES
+# module_level_variable2 = 98765
+# """int: var  Module level variable documented inline."""
+#=============================================================================
+
+
+
+
+
+
+
+#=============================================================================
+# FUNCTIONS/METHODS
+# def func(param1, param2):
+#     """This function does something with the parameters.
+#     Args:
+#         param1 (int): The first parameter.
+#         param2 (str): The second parameter.
+#     Returns:
+#         bool:  The return value. True for success, False otherwise.
+#         If function does not return a computed value, indicate
+#         "no return value" or "None"
+#    """
+#    function body statements
+#=============================================================================
+
+
+
+
+
+
+
+
+#=============================================================================
+# MAIN FUNCTION & TESTING AREA
+#
+# Optional test functions:
+# def test_xxx(...)
+#     """xxx is the name of the function you are testing
+#        document any arguments and return values as normal functions
+#     """
+#=============================================================================
+
+def main():
+    """Description of main() - what does this function do?  Does it run a
+	program?  Does it execute test code?"""
+
+
+
+
+if __name__ == '__main__':
+    main()
